3f22c1338b67e81eb232029157f081fdcf0514a3
Crab <crab@quantoptiongroup.com>
2024-04-15 23:22:33 -0700

    Updates

--- precache-manifest.03901c24c23c8678114b960c0e07602d.js
+++ precache-manifest.03901c24c23c8678114b960c0e07602d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4fa89bb3a1cfd274612d966dbd97ce38",
+    "revision": "7b23f6023f5a9ce4da5e1ca261f0cfaa",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.f01b737b.chunk.css"
   },
   {
-    "revision": "719513ca4bff9d8e135a",
-    "url": "/static/css/main.c321d7d4.chunk.css"
+    "revision": "33807940259209236e87",
+    "url": "/static/css/main.76cc6205.chunk.css"
   },
   {
     "revision": "3cbe2ed94088b21f2c29",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.c0fb4a7f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "719513ca4bff9d8e135a",
+    "revision": "33807940259209236e87",
     "url": "/static/js/main.6ec046c8.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95e3b6dbabba75d02f46002878cfd5a2.js"
+  "/precache-manifest.03901c24c23c8678114b960c0e07602d.js"
 );
 
 self.addEventListener('message', (event) => {
