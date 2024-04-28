9a9810d0f29c4636d911f99b7bc0eb60fb13b64f
Crab <crab@quantoptiongroup.com>
2024-04-28 13:14:33 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1e4c9a39d9eb04f8186a344db597a858.js"
+  "/precache-manifest.0d5518c80dcd4507801d5bb7910f4e31.js"
 );
 
 self.addEventListener('message', (event) => {
