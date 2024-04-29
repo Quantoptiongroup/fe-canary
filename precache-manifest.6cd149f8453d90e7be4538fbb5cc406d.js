e3114d36164bfacaa6e519917c666ab022cc6d9c
Crab <crab@quantoptiongroup.com>
2024-04-29 15:37:04 -0700

    Updates

--- precache-manifest.6cd149f8453d90e7be4538fbb5cc406d.js
+++ precache-manifest.6cd149f8453d90e7be4538fbb5cc406d.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c8ea7359f9d0c30c019dbce263a82db8",
+    "revision": "93bcbb2f85a912a23cfea8779cb35970",
     "url": "/index.html"
   },
   {
-    "revision": "b02fdaf8393ac771fcb6",
+    "revision": "03c4b4b277fbc351a603",
     "url": "/static/css/2.f01b737b.chunk.css"
   },
   {
-    "revision": "801a71c8cd537eb390dc",
-    "url": "/static/css/main.32a14e78.chunk.css"
+    "revision": "733e987ed38e89caeab0",
+    "url": "/static/css/main.ad6b0736.chunk.css"
   },
   {
-    "revision": "b02fdaf8393ac771fcb6",
-    "url": "/static/js/2.f5912079.chunk.js"
+    "revision": "03c4b4b277fbc351a603",
+    "url": "/static/js/2.da6cb422.chunk.js"
   },
   {
     "revision": "82085174ba6ed216ecb09c7a11d8bf17",
-    "url": "/static/js/2.f5912079.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.da6cb422.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "801a71c8cd537eb390dc",
-    "url": "/static/js/main.97d63b31.chunk.js"
+    "revision": "733e987ed38e89caeab0",
+    "url": "/static/js/main.e4dde26b.chunk.js"
   },
   {
     "revision": "04b2f78c2e282ee1297a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.df4c409c0f4f46dfd147ffd4f7ce12e9.js"
+  "/precache-manifest.6cd149f8453d90e7be4538fbb5cc406d.js"
 );
 
 self.addEventListener('message', (event) => {
